a2ca69c41bd86fc7bb89140fc6f39aa00c04460c
aviranabramov <108701368+aviranabramov@users.noreply.github.com>
2022-11-04 12:39:21 +0200

    add calculateBMR function
    The BMR is based on the gender, weight, height and the age of a person

--- script.js
+++ script.js
@@ -12,4 +12,15 @@ calculateBtn.addEventListener("click", () => {
   const BMI = calculateBMI(178, 86).toFixed(2);
   // TODO: Change according to the real data
   console.log(`BMI: ${BMI}`);
+
+  // BMR Calculator
+  const calculateBMR = (gender, weight, height, age) => {
+    if (gender === "male") {
+      return Math.round(66.5 + 13.75 * weight + 5.003 * height - 6.755 * age);
+    } else if (gender === "female") {
+      return Math.round(655.1 + 9.563 * weight + 1.85 * height - 4.676 * age);
+    }
+  };
+  // TODO: Change according to the real data
+  console.log(`BMR: ${calculateBMR("male", 86, 178, 26)}`);
 });
